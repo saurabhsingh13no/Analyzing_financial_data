677117b7180ffdc844d5854fc79637d92209b644
saurabhsingh13nov <singh.saurab@husky.neu.edu>
2017-06-07 15:29:32 -0400

    Implemented Neural Network Regressor and Random Forest Regressor

---
+++ test.py
@@ -0,0 +1,33 @@
+# import logging
+# import sys
+#
+# logger=logging.getLogger(__name__)
+# logger.setLevel(logging.INFO)
+#
+# formatter=logging.Formatter('%(asctime)s:%(levelname)s:%(name)s:%(message)s')
+#
+# file_handler=logging.FileHandler('test.log')
+# file_handler.setFormatter(formatter)
+#
+# logger.addHandler(file_handler)
+#
+# LEVELS = {'debug': logging.DEBUG,
+#           'info': logging.INFO,
+#           'warning': logging.WARNING,
+#           'error': logging.ERROR,
+#           'critical': logging.CRITICAL}
+#
+#
+# # if len(sys.argv) > 1:
+# #     level_name = sys.argv[1]
+# #     level = LEVELS.get(level_name, logging.NOTSET)
+# #     logging.basicConfig(level=level)
+#
+# logger.debug('This is a debug message')
+# logger.info('This is an info message')
+# logger.warning('This is a warning message')
+# logger.error('This is an error message')
+# logger.critical('This is a critical error message')
+
+import datetime
+print (datetime.datetime.strptime('2010-04-10','%Y-%m-%d'))
